051d0b3d1ff473d3ef09fa61feda5cad81276115
Chuks <chuksbestway2@gmail.com>
2024-02-23 19:23:35 +0100

    made some data dynamic

---
+++ data.js
@@ -0,0 +1,23 @@
+// object variable
+const clientReview = [
+  {
+    name: "ALEX HORMOZI THOMAS",
+    text: "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+    title: "Chairman",
+    src: "./images/man-2.jpg",
+  },
+
+  {
+    name: "JAMES PATTERSON T.",
+    text: "The thing about money is that it makes your life easier; if you're lucky to have it, you're lucky.",
+    title: "Author, Writer",
+    src: "./images/man-3.png",
+  },
+
+  {
+    name: "ROBERT KIYOSAKI",
+    text: "There is a difference between being poor and being broke. Broke is temporary, Poor is eternal.",
+    title: "Managing Director",
+    src: "./images/man-1.png",
+  },
+];
--- script.js
+++ script.js
@@ -9,9 +9,9 @@ showHamburger.addEventListener("click", () => {
     hideHamburger.style.display = "block";
     showHamburger.style.display = "none";
   } else {
-    navLinks.style.display = "none";
-    hideHamburger.style.display = "none";
-    showHamburger.style.display = "block";
+    navLinks.style.display = "block";
+    hideHamburger.style.display = "block";
+    showHamburger.style.display = "none";
   }
 });
 
@@ -22,3 +22,30 @@ hideHamburger.addEventListener("click", () => {
     showHamburger.style.display = "block";
   }
 });
+
+// grab the html content
+const reviewHeader = document.querySelector(".review-box-header");
+
+// map through the elements
+const displayReviewHeader = clientReview
+  .map(
+    (review) =>
+      `
+      <div class="review-box">
+          <i class="fa-solid fa-quote-left"></i>
+          <p>${review.text}</p>
+
+          <div class="review-box-image">
+            <div>
+              <img src="${review.src}" alt="" />
+            </div>
+            <p>${review.name}</p>
+            <p>${review.title}</p>
+          </div>
+      </div>
+    `
+  )
+  .join("");
+
+// set the innerHTML to display the content
+reviewHeader.innerHTML = displayReviewHeader;
